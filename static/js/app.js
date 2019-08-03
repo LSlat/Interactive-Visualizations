46063785dda9361fdc5ab4a999bd1249863e1314
LSlat <49042469+LSlat@users.noreply.github.com>
2019-08-03 17:22:09 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -15,73 +15,72 @@ function buildMetadata(sample) {
        Object.entries(data).forEach(([key, value]) => {
        choose.append('h6').text(`${key}: ${value}`);
       });
-  });
-
+      
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    buildGauge(data.WFREQ);
+  });
+  function buildGauge(WFREQ){
     // 
-    // var level = data['WFREQ']
+    var level = parseFloat(WFREQ)
 
-    // // Trig to calc meter point
-    // var degrees = 180 - level,
-    //     radius = .5;
-    // var radians = degrees * Math.PI / 180;
-    // var x = radius * Math.cos(radians);
-    // var y = radius * Math.sin(radians);
+    // Trig to calc meter point
+    var degrees = 180 - level,
+        radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
 
-    // // Path: may have to change to create a better triangle
-    // var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-    //     pathX = String(x),
-    //     space = ' ',
-    //     pathY = String(y),
-    //     pathEnd = ' Z';
-    // var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
 
-    // var data = [{ type: 'scatter',
-    //   x: [0], y:[0],
-    //     marker: {size: 28, color:'850000'},
-    //     showlegend: false,
-    //     name: 'speed',
-    //     text: level,
-    //     hoverinfo: 'text+name'},
-    //   { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
-    //   rotation: 90,
-    //   text: ['8-9','7-8','6-7','5-6','4-5', '3-4', '2-3', '1-2',
-    //             '0-1', ''],
-    //   textinfo: 'text',
-    //   textposition:'inside',
-    //   marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-    //                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-    //                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-    //                         'rgba(255, 255, 255, 0)','rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-    //                         'rgba(170, 202, 42, .5)']},
-    //   labels: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-    //   hoverinfo: 'label',
-    //   hole: .5,
-    //   type: 'pie',
-    //   showlegend: false
-    // }];
+    var data = [{ type: 'scatter',
+      x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'speed',
+        text: level,
+        hoverinfo: 'text+name'},
+      { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+      rotation: 90,
+      text: ['8-9','7-8','6-7','5-6','4-5', '3-4', '2-3', '1-2',
+                '0-1', ''],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                            'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                            'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                            'rgba(255, 255, 255, 0)','rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                            'rgba(255, 255, 255, 0)']},
+      labels: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
 
-    // var layout = {
-    //   shapes:[{
-    //       type: 'path',
-    //       path: path,
-    //       fillcolor: '850000',
-    //       line: {
-    //         color: '850000'
-    //       }
-    //     }],
-    //   title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
-    //   height: 500,
-    //   width: 500,
-    //   xaxis: {zeroline:false, showticklabels:false,
-    //             showgrid: false, range: [-1, 1]},
-    //   yaxis: {zeroline:false, showticklabels:false,
-    //             showgrid: false, range: [-1, 1]}
-    // };
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {color: '850000'}
+        }],
+      title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+      height: 400,
+      width: 400,
+      xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
+    };
+    Plotly.newPlot('gauge', data, layout);
+}
 
-// //     Plotly.newPlot('gauge', data, layout);
-// // }
 // );
 };
 
@@ -102,7 +101,7 @@ function buildCharts(sample) {
       hovertext: top_labels,
       type: "pie"
       }];
-      var layout = {height: 750, width: 800};
+      var layout = {height: 550, width: 575};
     
       Plotly.newPlot("pie", data2, layout);      
       
@@ -113,94 +112,12 @@ function buildCharts(sample) {
       
         var trace1 = [{
         x: x_value,
-        y: y_value,
+        y: y_value,        
         text: hover_labels,
         mode: 'markers',
         marker: {
-          color: x_value, 
-          // [
-          //   ' aliceblue ',
-          //   ' aqua ',
-          //   ' aquamarine ',
-          //   ' azure ',
-          //   ' beige ',
-          //   ' rosybrown ',
-          //   ' saddlebrown ',
-          //   ' salmon ',
-          //   ' blue ',
-          //   ' blueviolet ',
-          //   ' brown ',
-          //   ' burlywood ',
-          //   ' cadetblue ',
-          //   ' chartreuse ',
-          //   ' chocolate ',
-          //   ' coral ',
-          //   ' sandybrown ',
-          //   ' royalblue ',
-          //   ' crimson ',
-          //   ' cyan ',
-          //   ' mediumblue ',
-          //   ' mediumorchid ',
-          //   ' mediumpurple ',
-          //   ' mediumseagreen ',
-          //   ' mediumslateblue ',
-          //   ' mediumspringgreen ',
-          //   ' mediumturquoise ',
-          //   ' mediumvioletred ',
-          //   ' midnightblue ',
-          //   ' cornflowerblue ',
-          //   ' mistyrose ',
-          //   ' moccasin ',
-          //   ' red ',
-          //   ' navy ',
-          //   ' purple ',
-          //   ' olive ',
-          //   ' olivedrab ',
-          //   ' orange ',
-          //   ' orangered ',
-          //   ' peachpuff ',
-          //   ' peru ',
-          //   ' yellow ',
-          //   ' plum ',
-          //   ' dodgerblue ',
-          //   ' firebrick ',
-          //   ' violet ',
-          //   ' forestgreen ',
-          //   ' fuchsia ',
-          //   ' gainsboro ',
-          //   ' turquoise ',
-          //   ' gold ',
-          //   ' goldenrod ',
-          //   ' gray ',
-          //   ' grey ',
-          //   ' green ',
-          //   ' greenyellow ',
-          //   ' honeydew ',
-          //   ' hotpink ',
-          //   ' indianred ',
-          //   ' indigo ',
-          //   ' tomato ',
-          //   ' khaki ',
-          //   ' lavender ',
-          //   ' lavenderblush ',
-          //   ' lawngreen ',
-          //   ' pink ',
-          //   ' seagreen ',
-          //   ' deepskyblue',
-          //   ' sienna ',
-          //   ' palevioletred ',
-          //   ' skyblue ',
-          //   ' slateblue ',
-          //   ' slategray ',
-          //   ' slategrey ',
-          //   ' yellowgreen ',
-          //   ' springgreen ',
-          //   ' steelblue ',
-          //   ' tan ',
-          //   ' teal ',
-          //   ' thistle ',           
-          //   ' limegreen '           
-          //  ],             
+          color: x_value,
+          colorscale: "Earth",                
           size: y_value,
           sizemode: 'area',
           // sizeref: (2.* max(y_value)/(y_value**2)),
@@ -209,7 +126,7 @@ function buildCharts(sample) {
           sizemin: 10          
         }
       }];
-      var layout = {height: 600, width: 1200};
+      var layout = {height: 500, width: 1400, xaxes: {range: [0, 3200]}};
         
       Plotly.newPlot('bubble', trace1, layout)
      });
@@ -233,6 +150,7 @@ function init() {
     console.log("BuildCharts with firstSample")
     buildCharts(firstSample);
     buildMetadata(firstSample);
+    buildGauge(firstSample);
   });
 }
 
@@ -240,6 +158,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
+  buildGauge(newSample);
 }
 
 // Initialize the dashboard
